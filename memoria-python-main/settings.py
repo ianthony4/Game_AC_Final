caa0318e5ca2c98c9356227c2f1a687e9c15afd7
ianthony4 <achaisa@unsa.edu.pe>
2024-01-29 18:19:43 -0500

    Game v0.9

---
+++ memoria-python-main/settings.py
@@ -0,0 +1,29 @@
+# Dimensiones de la pantalla
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# Colores
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (206, 206, 206)
+BLUE = (30, 136, 229)
+
+# Configuraciones de los cuadros
+CARD_WIDTH = 200
+CARD_HEIGHT = 200
+
+# Archivos de sonido
+SOUND_BACKGROUND = "assets/fondo.wav"
+SOUND_CLICK = "assets/clic.wav"
+SOUND_SUCCESS = "assets/ganador.wav"
+SOUND_FAILURE = "assets/equivocado.wav"
+SOUND_FLIP = "assets/voltear.wav"
+
+# Archivo de imagen oculta
+HIDDEN_IMAGE_FILE = "assets/oculta.png"
+
+# Tiempo para mostrar una pareja incorrecta
+SHOW_PAIR_DELAY = 2
+
+# Altura del botón
+BUTTON_HEIGHT = 30
